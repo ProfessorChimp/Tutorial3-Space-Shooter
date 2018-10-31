f04a17e40ccc9cacf1a7c8fa49aaa6bbe4b0384d
ProfessorChimp <40414867+ProfessorChimp@users.noreply.github.com>
2018-10-31 00:13:33 -0400

    Completed Project
    There is a bug where Player does not explode

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController1 : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private Rigidbody rb;
+    private float nextFire;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Jump") &&Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            //          GameObject clone =
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);// as GameObject;
+            GetComponent<AudioSource>().Play(); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+             0.0f,
+             Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
